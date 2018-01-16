f11a9448a48281d57e0233948bc8b1b363811bf6
zmm <1614180462@qq.com>
2018-01-16 10:27:57 +0800

    js Reflect反射

---
+++ src/es6/class.js
@@ -0,0 +1,14 @@
+class D {
+    constructor(){
+        this.x = "123";
+        this.y = "abc";
+    }
+    console(){
+        console.log("console methods")
+    }
+
+    alert(){
+        console.log("alert methods")
+    }
+}
+
