11f442781cb5b5c6d146c75544c047e3a8f4176a
zmm <1614180462@qq.com>
2018-01-16 10:30:38 +0800

    在ts中使用反射

--- tsc/reflect.ts
+++ tsc/reflect.ts
@@ -1,3 +1,30 @@
 /**
  * ts中的反射通过 Reflect这个方法是js自带的
+ * https://www.cnblogs.com/brookshi/p/6426732.html
  */
+
+class Test {
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    name: string;
+    flag: Symbol = Symbol('flag');
+
+    getName(): string {
+        return this.name;
+    }
+}
+
+let obj = new Test('123');
+
+// tslint:disable-next-line:no-console
+console.info(Reflect.has(obj, 'name')); // true
+console.info(Reflect.has(obj, 'flag')); // true
+// tslint:disable-next-line:no-console
+console.info(Reflect.has(obj, 'get')); // true
+console.info(Reflect.has(obj, 'toString')); // true
+for ( const p of Reflect.ownKeys(obj)){
+    // tslint:disable-next-line:no-console
+    console.info(p); // name, flag
+}
