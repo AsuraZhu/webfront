39a896caeebd90010a89c912e99ab53dcf6c3e57
zmm <1614180462@qq.com>
2018-01-17 12:10:20 +0800

    ajax

---
+++ src/网络/ajax.js
@@ -0,0 +1,38 @@
+/**
+ * ajajx() 可以兼容ie
+ * ActiveXObject 是ie6以下
+ * 简单的使用xmlhttprequest对象
+ */
+
+ // options 是{} 
+function ajax(options) {
+    const opt = {
+        type: "get",
+        url: "/",
+    };
+    const xhr = new XMLHttpRequest();
+    // 打开一个连接
+    xhr.open(options.type,options.url);
+    xhr.responseType = "json";
+    xhr.send();
+    // 
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState==4) {
+            if(xhr.status==200){
+                return xhr.response;
+            }
+        }
+        return xhr.response;
+    }
+}
+
+
+// 不考虑兼容性使用fetch
+
+fetch('url', {
+    method: "post",
+    body: {"methods":"test"}
+}).then(res => {
+   res.json(); 
+})
+
